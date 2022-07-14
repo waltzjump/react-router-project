4858f7a5d537df0fc3f40a5e5800aed6bf54aa43
kaden kirkwood <sniperclass10@gmail.com>
2022-07-14 16:13:59 -0600

    final commit

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # kadens react
-
-> Provided for the students of the [Bottega Code School](https://bottega.tech/)
-
-*Fork from [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
---
+++ src/components/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PortfolioContainer from "../portfolio/portfolio-containter";
+
+export default function() {
+    return (
+        <div>
+            <PortfolioContainer />
+        </div>
+    );
+}
